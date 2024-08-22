600dc57c8576b7b0ac921919087d930b2332d298
Youssef ElGohre <118435243+MEyoussef@users.noreply.github.com>
2024-08-22 06:45:36 -0700

    Update background.js

--- Keep My Session Alive/background.js
+++ Keep My Session Alive/background.js
@@ -3,7 +3,7 @@ const targetUrl = "Target URL";
 // Creat Alarm That Will Start After 5 Min And Will Work Every 5 Min
 chrome.alarms.create("keepAlive", { delayInMinutes: 5, periodInMinutes: 5 });
 
-// When Alarm Work Check if it's Name is KeepAlive and Then Look for Target Tab And Reload it
+// When Alarm Work Check if The Name is KeepAlive and Then Look for Target Tab And Reload it
 chrome.alarms.onAlarm.addListener((alarm) => {
     if (alarm.name === "keepAlive") {
         chrome.tabs.query({ url: targetUrl + "/*" }, (tabs) => {
